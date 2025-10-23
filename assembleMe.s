2f52e2ca3c7c56cf1d0bb59cbd613a6c36c1cbeb
LordLoading <lorenzdressel@gmail.com>
2025-10-23 19:51:57 +0200

    quicksave

--- assembleMe.s
+++ assembleMe.s
@@ -11,12 +11,27 @@ setup:
 strLoop:
     lb t0, 0(t1)
     addi t1, t1, 1
-    beq t0, x0, 0xffff
+    beq t0, x0, 8
+    j 8;
+    call keyboardSetup
     sb t0, 0(t2)
     call strLoop
+
+keyboardSetup:
+    li t3, keyboard
+    lw t3, 0(t3)
+    call keyboardLoop
+
+keyboardLoop:
+    lbu t0, 0(keyboard)
+    sb t0, 0(keyboard)
+    sb t2,
 
 msg:
     .string "alerta alerta antifacista!"
 
 terminal:
     .word 0x02000000
+
+keyboard:
+    .word 0x03000000
